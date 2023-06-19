27e10ac5143a0edf804de840a69003611411227a
SaiAbhiRam <saiabhiram1928@gamil.com>
2023-06-19 20:23:25 +0530

    modified : app.js routes

--- src/App.js
+++ src/App.js
@@ -22,10 +22,10 @@ const App = () => {
 <Routes>
   <Route path="/Explore" element={<Explore/>}/>
   <Route path="/" element={<Home/>}/>
-  <Route path="/Git-Hub-Finder" element ={<Home/>}/>
+  <Route path="/*" element ={<Home/>}/>
   <Route path='/users/:login' element={<User/>}/>
   <Route path="/Notfound"  element={<Error/>}/>
-  <Route path="/*"  element={<Home/>}/>
+  {/* <Route path="/*"  element={<Error/>}/> */}
 
   </Routes>
 </main>
